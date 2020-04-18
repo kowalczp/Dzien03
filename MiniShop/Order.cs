3c09fc421ab9794f97e51073aea03f67ace97096
pkowalczykk <pat.kowalczyk@wp.pl>
2020-04-18 17:05:54 +0200

    Commit end

---
+++ MiniShop/Order.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop
+{
+    enum OrderStatus
+    {
+        NewOrder,
+        Complete,
+        Confirmed,
+        Shipped,
+        Returned,
+        Canceled
+    }
+    class Order
+    {
+        private String orderNumber;
+        private List<OrderItem> items = new List<OrderItem>();
+
+        private byte discount = 0;
+        private String cutomerName;
+        private String shipAddress;
+        private DateTime orderDate;
+        private OrderStatus status = OrderStatus.NewOrder;
+
+
+        public bool AddProduct(Product product, int qnty)
+
+
+        {
+            if (status == OrderStatus.NewOrder)
+            {
+                items.Add(new OrderItem(product, qnty));
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public bool RemoveProduct(Product product)
+        {
+            if (status == OrderStatus.NewOrder)
+            {
+ 
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Clear()
+        {
+            if (status == OrderStatus.NewOrder)
+            {
+                items.Clear();
+ 
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private double CalcTotalAmount()
+        {
+            double amount = 0.0;
+
+            foreach (OrderItem item in items)
+            {
+                amount += item.ProductPrice * item.Qnty;
+            }
+            if (discount > 0 && discount <= 100)
+            {
+                amount += (100 - discount)/100.0;
+            }
+            return amount;
+        }
+        
+    }
+}
---
+++ MiniShop/OrderItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop
+{
+    class OrderItem
+    {
+        protected Product product;
+        protected int qnty;
+
+        protected double ProductPrice { get { return product.Price; } }
+        protected double Qnty { get { return product.Qnty; } }
+
+        public OrderItem(Product product, int qnty)
+        {
+            this.product = product;
+            this.qnty = qnty;
+        }
+    }
+}
